48eed9038ac080248a55247ee0c4b053ad900d58
BackdoorMind <ram946271@gmail.com>
2025-12-18 18:00:05 +0530

    update
    update

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ async def main():
     await bot.load_extension("cogs.autorole")
     await bot.load_extension("cogs.safety")
     await bot.load_extension("cogs.ticket")
-    await bot.load_extension("cogs.music")
     await bot.start(TOKEN)
 
 asyncio.run(main())

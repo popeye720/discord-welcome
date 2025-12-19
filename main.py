07fb7ffbef018beb9b09aa1b117c625b485fe4ce
BackdoorMind <ram946271@gmail.com>
2025-12-19 11:51:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def main():
     await bot.load_extension("cogs.welcome")
     await bot.load_extension("cogs.message")
     await bot.load_extension("cogs.autorole")
-    await bot.load_extension("cogs.safety")
+    #await bot.load_extension("cogs.safety")
     await bot.load_extension("cogs.ticket")
     await bot.start(TOKEN)
 

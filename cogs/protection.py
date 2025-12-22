71a177fa49b56cb876066c3828720522e72f19f5
BackdoorMind <ram946271@gmail.com>
2025-12-22 18:53:14 +0530

    protection

---
+++ cogs/protection.py
@@ -0,0 +1,59 @@
+import os
+import re
+import discord
+from discord.ext import commands
+
+LINK_REGEX = re.compile(
+    r"(https?:\/\/|www\.)\S+",
+    re.IGNORECASE
+)
+
+ALLOWED_CHANNEL_LINKS = int(os.getenv("ALLOWED_CHANNEL_LINKS", 0))
+OWNER_ID = int(os.getenv("OWNER_ID", 0))
+
+class Protection(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        # Ignore bots
+        if message.author.bot:
+            return
+
+        # Ignore DMs
+        if not message.guild:
+            return
+
+        # Owner can send links anywhere
+        if message.author.id == OWNER_ID:
+            return
+
+        # Allowed channel
+        if message.channel.id == ALLOWED_CHANNEL_LINKS:
+            return
+
+        # Detect link
+        if LINK_REGEX.search(message.content):
+            try:
+                await message.delete()
+            except discord.Forbidden:
+                return
+
+            # DM the user
+            try:
+                embed = discord.Embed(
+                    title="🚫 Link Not Allowed",
+                    description=(
+                        f"Hello **{message.author.name}**, 👋\n\n"
+                        "**Links are not allowed in this server.**\n\n"
+                        "📌 Please use the allowed links channel."
+                    ),
+                    color=discord.Color.red()
+                )
+                await message.author.send(embed=embed)
+            except discord.Forbidden:
+                pass  # User has DMs closed
+
+async def setup(bot):
+    await bot.add_cog(Protection(bot))

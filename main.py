03620e9fa070e82b2e196faf5069151599d23cf8
BackdoorMind <ram946271@gmail.com>
2025-12-25 22:18:42 +0530

    1

--- main.py
+++ main.py
@@ -69,7 +69,6 @@ async def main():
     await bot.load_extension("cogs.dm")
     await bot.load_extension("cogs.followuser")
     await bot.load_extension("cogs.stream")
-    #await bot.load_extension("cogs.clip")
     await bot.start(TOKEN)
 
 asyncio.run(main())

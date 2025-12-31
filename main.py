39e6be64d6b1cda95a983c84584717586304a047
BackdoorMind <ram946271@gmail.com>
2025-12-31 23:50:59 +0530

    1

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ async def main():
         await bot.load_extension("cogs.moderation")
         await bot.load_extension("cogs.joinleave")
         await bot.load_extension("cogs.voice_speak")
-
+        await bot.load_extension("cogs.ffmpeg")
         await bot.start(TOKEN)
 
 

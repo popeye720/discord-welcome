aac7a946450707c8eaaf5b26d2e04822947b73cb
BackdoorMind <ram946271@gmail.com>
2025-12-31 14:31:51 +0530

    1

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import asyncio
 from database.mongo import db
 import shutil
 print("[DEBUG] ffmpeg path:", shutil.which("ffmpeg"))
-import subprocess
-subprocess.run(["ffmpeg", "-version"])
+
 
 # ✅ ALLOWED SERVERS
 ALLOWED_GUILD_IDS = {

9ddccd97d3f6fdfaa4d071c6faa37ba094ab28e0
BackdoorMind <ram946271@gmail.com>
2026-01-01 15:00:33 +0530

    1

--- database/models.py
+++ database/models.py
@@ -1,12 +1,9 @@
 from .mongo import db
 
-autorole_col = db["autorole"]
+
 jtc_col = db["join_to_create"]
 ticket_col = db["tickets"]
 reactionrole_col = db["reaction_roles"]
 autotrigger_col = db["auto_triggers"]
 
-yt_notify_col = db["yt_notify"]
-yt_last_col = db["yt_last"]
 
-speak_roles_col = db["speak_roles"]

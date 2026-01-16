4022e4504f52076a5b483df8c5dc555124301cd2
RE350 <122217092+popeye720@users.noreply.github.com>
2026-01-16 21:37:31 +0530

    Update autoping.py

--- cogs/autoping.py
+++ cogs/autoping.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 from datetime import datetime
 from database.mongo import db
-from database.models import autoping_rules
+from database.models import autoping_col
 
 
 
